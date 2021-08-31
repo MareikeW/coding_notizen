e2bb63b4083eaf7dc9ac78fb12574361c1047f75
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-31 19:05:32 +0200

    async javascript

---
+++ api/07.js
@@ -0,0 +1,10 @@
+/**
+ * Challenge: Define our anonymous callback function as a separate function, then
+ * pass it as the 2nd parameter to our `addEventListener`
+ */
+
+document.getElementById("new-deck").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
---
+++ api/08.js
@@ -0,0 +1,8 @@
+/**
+ * Challenge: 
+ * 
+ * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
+ * 
+ * Part 2: Move the anonymous in-line function to its own, named function
+ */
+
